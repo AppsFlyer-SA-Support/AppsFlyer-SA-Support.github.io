3025ccf37bfcb90b72ad2d7245f9d381f1d5e81f
khangh <107156118+AppsFlyer-SA-Support@users.noreply.github.com>
2024-02-26 14:02:18 -0800

    Update App.js

--- my-jsonforms-app/src/App.js
+++ my-jsonforms-app/src/App.js
@@ -8,9 +8,52 @@ import {
 import React, { useState } from 'react';
 import { JsonForms } from '@jsonforms/react';
 
-const schema = person.schema;
-const uischema = person.uischema;
-const initialData = person.data;
+const schema = {
+  type: 'object',
+  properties: {
+    datalocker: {
+      title: 'Data Locker Re-run',
+      type: 'object',
+      properties: {
+        clientId: {
+          type: 'string',
+        },
+        subscriptionId: {
+          type: 'string',
+        },
+        comment: {
+          type: 'string',
+        },
+        zenDeskTicket: {
+          type: 'string',
+        },
+        topics: {
+          type: "array",
+          uniqueItems: true,
+          items: {
+            type: "string",
+            enum: ['', 'impressions', 'inapps', 'sessions', 'organicUninstalls', 'uninstalls', 'installs',
+            'clicks', 'inappsRetargeting', 'conversionsRetargeting', 'sessionsRetargeting', 'clicksRetargeting', 'impressionsRetargeting',
+            'cohortUserAcquisition', 'cohortRetargeting', 'cohortUnified', 'websiteEvents', 'websiteVisits', 'websiteAssistedInstalls',
+            'reinstalls', 'organicReinstalls', 'blockedClicks', 'blockedInapps', 'blockedInstalls', 'postAttributionInstalls', 'skadPostbacks',
+            'skadAdvertiserPostbacks', 'skadConversionValueChangelog', 'skadInstalls', 'skadInapps', 'skadSrnAdvertisingDimensions',
+            'skadRedownloads', 'incrementalityInapps', 'incrementalityFirstSeenUsers', 'incrementalitySessions', 'incrementalityInappsReattr',
+            'incrementalitySessionsReattr', 'incrementalityImpressions', 'incrementalityImpressionsRetarget', 'incrementalityOrganicSessions',
+            'incrementalityOrganicInapps', 'attributedAdRevenue', 'organicAdRevenue', 'retargetingAdRevenue', 'attributedAdRevenueV2',
+            'organicAdRevenueV2', 'retargetingAdRevenueV2', 'attributedAdrevenueSdkImpressionLevel', 'storesRawdata', 'storesDevicedata',
+            'cohortUserAcquisitionVersioned', 'adnetCohort', 'adnetAdRevenue', 'failedClickSignatures', 'blockedClicksImpressions',
+            'rawPostbacks', 'aggregatedPostbacks', 'cohortUserAcquisition', 'cohortRetargeting', 'cohortUnified', 'cohortUserAcquisitionVersioned',
+            'cohortRetargetingVersioned', 'cohortUnifiedVersioned', 'cohortUserAcquisitionTimezoneVersioned', 'cohortRetargetingTimezoneVersioned',
+            'cohortUnifiedTimezoneVersioned', 'cohortUnifiedAgency', 'cohortRetargetingAgency', 'cohortUserAcquisitionAgency']
+        }
+      }
+    },
+      required: ['clientId', 'subscriptionId'],
+    }
+  }
+};
+const uischema = datalocker.uischema;
+const initialData = datalocker.data;
 
 function App() {
   const [data, setData] = useState(initialData);
